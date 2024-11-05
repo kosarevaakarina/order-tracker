f6910af6c39c6080ce319ce67dbbe30830c4bf35
kosarevaakarina <kosareva.karina2013@yandex.ru>
2024-11-05 19:41:57 +0300

    fix logger

--- src/config/logger.py
+++ src/config/logger.py
@@ -5,11 +5,15 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-def create_handler(file_name, level):
+def create_handler(file_name, level, filter_error=False):
     handler = logging.FileHandler(file_name, encoding='utf-8')
     handler.setLevel(level)
     handler.setFormatter(formatter)
+
+    if filter_error:
+        handler.addFilter(lambda record: record.levelno < logging.ERROR)
+
     return handler
 
 logger.addHandler(create_handler('error.log', logging.ERROR))
-logger.addHandler(create_handler('debug.log', logging.DEBUG))
+logger.addHandler(create_handler('debug.log', logging.DEBUG, filter_error=True))
