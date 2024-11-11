0b9455e8445e2175d23f51aa63e1eec332b22ab0
kosarevaakarina <kosareva.karina2013@yandex.ru>
2024-11-11 20:48:25 +0300

    fix kafka

--- src/crud/order_crud.py
+++ src/crud/order_crud.py
@@ -1,21 +1,17 @@
-from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from config.logger import logger
-from exceptions import PermissionDeniedException, OrderNotFoundException
 from models.orders import Order
 from models.users import User
-from schemas.order_schema import OrderCreate, OrderUpdateStatus
 
 
 class OrderCrud:
     @staticmethod
-    async def create_order(order_data: OrderCreate, current_user: User, session: AsyncSession):
+    async def create_order(order_data: dict, current_user: User, session: AsyncSession):
         new_order = Order(
-            title=order_data.title,
-            description=order_data.description,
-            status=order_data.status,
-            price=order_data.price,
+            title=order_data.get("title"),
+            description=order_data.get("description"),
+            status=order_data.get("status"),
+            price=order_data.get("price"),
             user_id=current_user.id
         )
         session.add(new_order)
@@ -39,19 +35,10 @@ class OrderCrud:
         return order
 
     @staticmethod
-    async def update_status_order(session: AsyncSession, order_id: int, order_data: OrderUpdateStatus, current_user: User):
+    async def update_status_order(session: AsyncSession, order_id: int, order_data: dict):
         order = await OrderCrud.get_order(order_id, session)
-        if not (current_user.is_superuser or current_user.id == order.user_id):
-            logger.error("FORBIDDEN: Insufficient permissions to change the status of order ID=%s for user ID=%s", order.id, current_user.id)
-            raise PermissionDeniedException()
-        if order.status == order_data.status:
-            logger.warning("Conflict: Failed to change the status for order ID=%s", order_id)
-            raise HTTPException(status_code=409, detail="Conflict: status not changed")
-        if order:
-            order.status = order_data.status
-            session.add(order)
-            await session.commit()
-            await session.refresh(order)
-            return order
-        else:
-            raise OrderNotFoundException(order_data.id)
+        order.status = order_data.get("status")
+        session.add(order)
+        await session.commit()
+        await session.refresh(order)
+        return order
