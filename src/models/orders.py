73273c13bb83de4e79eddc256caf406cda40c9e0
kosarevaakarina <kosareva.karina2013@yandex.ru>
2024-11-11 14:06:50 +0300

    add price to order

--- src/models/orders.py
+++ src/models/orders.py
@@ -1,8 +1,8 @@
 import enum
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship, validates
 from config.db import Base
 
 
@@ -22,3 +22,10 @@ class Order(Base):
     owner = relationship("User", back_populates='orders')
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    price = Column(Float)
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if value <= 0:
+            raise ValueError("Price must be greater than 0")
+        return value
--- src/schemas/order_schema.py
+++ src/schemas/order_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, field_validator
 from typing import Optional
 from models.orders import OrderStatus
 
@@ -7,6 +7,13 @@ class OrderCreate(BaseModel):
     title: str = Field(..., min_length=1, max_length=100)
     description: Optional[str] = Field(None, max_length=255)
     status: OrderStatus = Field(default=OrderStatus.pending)
+    price: float
+
+    @field_validator('price')
+    def validate_price(cls, value):
+        if value <= 0:
+            raise ValueError("Price must be greater than 0")
+        return value
 
 
 class OrderInfo(OrderCreate):
